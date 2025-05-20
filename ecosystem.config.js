80ceb7b8faa367df6705975829028eb085efdaec
Mklop0098 <hoangsonfds@gmail.com>
2025-05-21 06:27:28 +0700

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,27 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
+  apps: [{
+    name: 'bksport_sv',
+    script: 'dist/server.js',
+    watch: '.',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 3001
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    }
   }],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+  deploy: {
+    production: {
+      user: 'root',
+      host: '128.199.253.91',
+      ref: 'origin/main',
+      repo: 'git@github.com:Mklop0098/bksport_sv.git',
+      path: '/var/www/bksport_sv',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
